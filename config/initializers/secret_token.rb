a4b0069488ea0dfab50be464878e39e944a9f871
Rodrigo Silva <rodrigo_rbs@yahoo.com.br>
2012-03-24 21:14:51 -0300

    Novo Ereds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ereds::Application.config.secret_token = '7431ada26e613961fce69814465fc184ab8c8adcfcccc0200031c1f82f9a7977a6b9dcaeec3e032a24793e86b6c75bcb74aae0885788bfd810b72606aea381b4'
